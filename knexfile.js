6e5d5e87a952d72c695b03bdc2cc8c1432c5e72c
FaiiFaiipuipui <pakarnpahulo2003@gmail.com>
2024-08-02 23:39:55 +0700

    feat!: Add Database & Initialization file - add a docker compose file for postgres + pgadmin - add a database migration (initialization) file run `knex migrate:latest` to activate - use knex for sql query building

---
+++ knexfile.js
@@ -0,0 +1,26 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: "pg",
+    connection: {
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_HOST_PORT,
+      user: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_USER_PASSWORD,
+      database: process.env.DATABASE_NAME,
+    },
+    pool: { min: parseInt(process.env.DATABASE_POOL_MIN), max: parseInt(process.env.DATABASE_POOL_MAX) },
+    acquireConnectionTimeout: 10000,
+    migrations: {
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    }
+  }
+};
