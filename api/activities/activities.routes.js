12ac461e9e71ed773d4528c67af4ba9766a2af82
tatatitutatuay <thirathawat996@gmail.com>
2024-10-16 15:36:49 +0700

    feat(api): add auth of createActivity

--- api/activities/activities.routes.js
+++ api/activities/activities.routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const activitiesRoute = express.Router({ mergeParams: true });
 const versionMiddleware = require('../../middleware/versionMiddleware');
+const {protect, authorize} = require('../../middleware/auth');
 
 // Handlers
 const createActivity = require('./createActivity');
 
-// create a semester
-activitiesRoute.post('/', versionMiddleware(1), createActivity.v1);
+// create a new activity
+activitiesRoute.post('/', versionMiddleware(1), protect, authorize('applicationAdmin'), createActivity.v1);
 
 // export
 module.exports = activitiesRoute;
