aedffba4a6e23fb2057c86ef9af38f21bf812ca8
tatatitutatuay <thirathawat996@gmail.com>
2024-10-17 00:01:56 +0700

    feat(utils): getCompanies can send params

--- utils/getCompanies.js
+++ utils/getCompanies.js
@@ -1,7 +1,12 @@
-exports.getCompanies = function getCompanies() {
+exports.getCompanies = function getCompanies(page, limit, search) {
     const axios = require('axios');
 
     const companies = axios.get('https://cedtintern.cp.eng.chula.ac.th/api/companies', {
+        params: {
+            page: page || 1,
+            limit: limit || 25,
+            search: search || ''
+        },
         headers: {
             'accept': 'application/json, text/plain, */*',
             'cookie': 'token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic3R1ZGVudCIsInBsYXRmb3JtVWlkIjoiNEZvQnRFOVJkc1hsSHVUMmpuMVhRMHNSNGlJcjE1ZEFRc1ZHNmQ2cCIsInN0dWRlbnRJZCI6IjY2MzMwODU2MjEiLCJpYXQiOjE3MjkwNjk0MjYsImV4cCI6MTcyOTE1NTgyNiwiaXNzIjoiQ0VEVCBJbnRlcm4gUG9ydGFsIEFQSSJ9.3AIyh3yIOAU7C_QD-TKQgbbedoXMheJnjyyaZRTNrn4',
