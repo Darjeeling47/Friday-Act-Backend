a7257ae54da753d68942719b3a57f27f060675b6
tatatitutatuay <thirathawat996@gmail.com>
2024-10-17 17:39:55 +0700

    feat(api): deleteActivity

--- api/activities/activities.routes.js
+++ api/activities/activities.routes.js
@@ -8,12 +8,14 @@ const createActivity = require('./createActivity');
 const getActivities = require('./getActivities');
 const getActivity = require('./getActivity');
 const updateActivity = require('./updateActivity');
+const deleteActivity = require('./deleteActivity');
 
 // create a new activity
 activitiesRoute.post('/', versionMiddleware(1), protect, authorize('applicationAdmin'), createActivity.v1);
 activitiesRoute.get('/', versionMiddleware(1), protect, getActivities.v1);
 activitiesRoute.get('/:id', versionMiddleware(1), protect, getActivity.v1);
 activitiesRoute.put('/:id', versionMiddleware(1), protect, authorize('applicationAdmin'), updateActivity.v1);
+activitiesRoute.delete('/:id', versionMiddleware(1), protect, authorize('applicationAdmin'), deleteActivity.v1);
 
 // export
 module.exports = activitiesRoute;
---
+++ api/activities/deleteActivity/deleteActivity.handler.js
@@ -0,0 +1,3 @@
+const deleteActivityV1 = require('./deleteActivity.v1');
+
+exports.v1 = deleteActivityV1;
