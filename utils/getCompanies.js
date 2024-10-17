98a4af9d22278cb192da6c61e9e24cfd660e6d8f
tatatitutatuay <thirathawat996@gmail.com>
2024-10-17 17:21:18 +0700

    fix(utils): getCompanies bug

--- utils/getCompanies.js
+++ utils/getCompanies.js
@@ -7,7 +7,7 @@ exports.getCompanies = function getCompanies(search) {
         },
         headers: {
             'accept': 'application/json, text/plain, */*',
-            'cookie': 'token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic3R1ZGVudCIsInBsYXRmb3JtVWlkIjoiNEZvQnRFOVJkc1hsSHVUMmpuMVhRMHNSNGlJcjE1ZEFRc1ZHNmQ2cCIsInN0dWRlbnRJZCI6IjY2MzMwODU2MjEiLCJpYXQiOjE3MjkwNjk0MjYsImV4cCI6MTcyOTE1NTgyNiwiaXNzIjoiQ0VEVCBJbnRlcm4gUG9ydGFsIEFQSSJ9.3AIyh3yIOAU7C_QD-TKQgbbedoXMheJnjyyaZRTNrn4',
+            'cookie': `token=${token}`,
         }
     })
     .catch(error => {
