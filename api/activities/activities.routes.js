79443eb2ff89bfc6070562c1211c83492a70f498
tatatitutatuay <thirathawat996@gmail.com>
2024-10-17 15:07:38 +0700

    feat(route): add route to getActivities

--- api/activities/activities.routes.js
+++ api/activities/activities.routes.js
@@ -5,9 +5,11 @@ const {protect, authorize} = require('../../middleware/auth');
 
 // Handlers
 const createActivity = require('./createActivity');
+const getActivities = require('./getActivities');
 
 // create a new activity
 activitiesRoute.post('/', versionMiddleware(1), protect, authorize('applicationAdmin'), createActivity.v1);
+activitiesRoute.get('/', versionMiddleware(1), protect, getActivities.v1);
 
 // export
 module.exports = activitiesRoute;
