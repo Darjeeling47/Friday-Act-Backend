270358ec0f1b3a6927f5dff49c73832ab108de13
tatatitutatuay <thirathawat996@gmail.com>
2024-10-21 16:20:50 +0700

    add route to getActivityParticipants

--- api/activities/activities.routes.js
+++ api/activities/activities.routes.js
@@ -9,6 +9,7 @@ const getActivities = require('./getActivities');
 const getActivity = require('./getActivity');
 const updateActivity = require('./updateActivity');
 const deleteActivity = require('./deleteActivity');
+const getActivityParticipants = require('./getActivityParticipants');
 
 // create a new activity
 activitiesRoute.post('/', versionMiddleware(1), protect, authorize('applicationAdmin'), createActivity.v1);
@@ -16,6 +17,7 @@ activitiesRoute.get('/', versionMiddleware(1), protect, getActivities.v1);
 activitiesRoute.get('/:id', versionMiddleware(1), protect, getActivity.v1);
 activitiesRoute.put('/:id', versionMiddleware(1), protect, authorize('applicationAdmin'), updateActivity.v1);
 activitiesRoute.delete('/:id', versionMiddleware(1), protect, authorize('applicationAdmin'), deleteActivity.v1);
+activitiesRoute.get('/:id/participants', versionMiddleware(1), protect, authorize('applicationAdmin'), getActivityParticipants.v1);
 
 // export
 module.exports = activitiesRoute;
