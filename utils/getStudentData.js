1feda0644379519f3f4b54ee77c2dc092fc81dee
tatatitutatuay <thirathawat996@gmail.com>
2024-10-21 14:36:15 +0700

    fix(utils): get student data

--- utils/getStudentData.js
+++ utils/getStudentData.js
@@ -1,40 +1,23 @@
+const axios = require('axios');
+
 exports.getStudentData = function getStudentData(token, studentId) {
-  try {
-    if (!studentId || studentId === undefined) {
-      return null
+  const options = {
+    method: 'GET',
+    url: 'https://cedtintern.cp.eng.chula.ac.th/api/internal/v1/students',
+    params: {
+      studentIds: studentId
+    },
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${token}`
     }
+  };
+  
+  const students = axios.request(options).then(function (response) {
+    console.log(response.data);
+  }).catch(function (error) {
+    console.error(error);
+  });
 
-    const apiUrl = 'https://cedtintern.cp.eng.chula.ac.th/api/internal/v1/students'
-
-    const queryParams = {
-      studentIds: [studentId, ''],
-      limit: 1
-    }
-
-    const queryString = new URLSearchParams(queryParams).toString();
-
-    const fullUrl = `${apiUrl}?${queryString}`;
-
-    fetch(fullUrl, {
-      headers: {
-        "accept": "application/json",
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${token}`
-      },
-    }).then((response) => {
-      if (!response.ok) {
-        throw new Error('Unable to Fetch information.')
-      }
-      const JSONresponse = response.json();
-
-      if (JSONresponse.meta.totalItem !== 1) {
-        throw new Error ('More than 1 student with this Id exists.')
-      }
-      
-      return JSONresponse.items[0];
-    });
-  } catch (error) {
-    console.error('Error: ', error);
-    return null;
-  }
+  return students;
 };
