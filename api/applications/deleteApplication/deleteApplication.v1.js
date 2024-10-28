040cb377f39a0f2336218baabc5c0444b76cbf4b
FaiiFaiipuipui <pakarnpahulo2003@gmail.com>
2024-10-29 02:13:22 +0700

    feat: Add delete application functionality
    This commit adds the functionality to delete an application. It checks if the application ID is valid and deletes the corresponding record from the database. If the application is not found, it returns a 404 error. If an error occurs during the deletion process, it returns a 500 error. Otherwise, it returns a 200 success status with an empty application object.

--- api/applications/deleteApplication/deleteApplication.v1.js
+++ api/applications/deleteApplication/deleteApplication.v1.js
@@ -1,1 +1,34 @@
-module.exports =  async (req, res, next) => {}
+module.exports = async (req, res, next) => {
+  try {
+    const applicationId = req.params.id;
+
+    const user = req.user;
+
+    if (!applicationId || typeof applicationId == "undefined" || typeof applicationId != "number") {
+      return res.status(404).json({
+        success: false,
+        message: "This application is not found.",
+      });
+    }
+
+    const application = await knex("APPLICATIONS")
+      .where({ id: applicationId })
+      .del("*");
+
+      if (!application) {
+        return res.status(404).json({
+          success: false,
+          message: "This application is not found.",
+        });
+      }
+
+
+    return res.status(200).json({
+      success: true,
+      application: {}
+    })
+  } catch {
+    console.log(error);
+    return res.status(500).json({ message: "An error occurred.", error: error.message });
+  }
+};
