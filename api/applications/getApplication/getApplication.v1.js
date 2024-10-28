e1732800dda9b90b25416742b3fe015ff463cb7c
FaiiFaiipuipui <pakarnpahulo2003@gmail.com>
2024-10-29 02:16:32 +0700

    feat: Add database query logic to retrieve application details
    **(AWAIT GET USER)**
    This commit adds the necessary database query logic to retrieve application details in the "getApplication.v1.js" file. It uses Knex.js to query the "APPLICATIONS" table based on the provided application ID. If the application is found, it constructs a response object with relevant details including the user, activity, and application attributes. If the application is not found, it returns a 404 error response. In case of any errors during the process, a 500 error response is returned.

--- api/applications/getApplication/getApplication.v1.js
+++ api/applications/getApplication/getApplication.v1.js
@@ -1,1 +1,54 @@
-module.exports =  async (req, res, next) => {}
+const knex = require("knex")(require("../../../knexfile").development);
+
+module.exports = async (req, res, next) => {
+  try {
+    const applicationId = req.params.id;
+
+    const application = await knex("APPLICATIONS")
+      .where({ id: applicationId })
+      .select("*")
+      .first();
+
+    if (!application) {
+      return res.status(404).json({
+        success: false,
+        message: "This application is not found.",
+      });
+    }
+
+    const userObj = null;
+
+    const activityObj = await knex("ACTIVITIES")
+      .where({ id: activityId })
+      .select("*")
+      .first();
+
+    const applicationRes = {
+      id: insertedApplication.id,
+      user: {
+        id: userObj.id,
+        thaiName: userObj.firstNameTh + " " + userObj.lastNameTh,
+        studentId: userObj.studentId,
+      },
+      activity: activityObj, // to remove unused attribute
+      createdAt: application.created_at, // ask if use inserted value
+      updatedAt: application.updated_at,
+      isQrGenerated: application.is_qr_generated,
+      qrString: application.qr_string,
+      qrGeneratedAt: application.qr_generated_at,
+      isApproved: application.is_approved,
+      isCanceled: application.is_canceled,
+      cancellationReason: application.cancellation_reason,
+    };
+
+    return res.status(200).json({
+      success: true,
+      application: applicationRes,
+    });
+  } catch {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ message: "An error occurred.", error: error.message });
+  }
+};
