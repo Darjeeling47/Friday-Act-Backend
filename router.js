fd906337270f2eeddee4a538945a79939d125e31
tatatitutatuay <thirathawat996@gmail.com>
2024-10-30 22:21:14 +0700

    feat: add route to image activities poster

--- router.js
+++ router.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 const api = require('./api');
+const path = require('path');
 
 router.use('/api/:version/semesters', api.semesters);
 router.use('/api/:version/tags', api.tags);
 router.use('/api/:version/activities', api.activities);
+
+router.use('/image/activities/poster', express.static(path.join(__dirname, 'image', 'activities', 'poster')));
 
 router.get('/', (req, res) => {
   res.send('main page')
