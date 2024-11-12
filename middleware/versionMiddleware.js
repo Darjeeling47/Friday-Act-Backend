c908efa19952a539aa66f8146833fd3cb27dd4ad
Darjeeling47 <supawit.ma@gmail.com>
2024-11-13 00:51:48 +0700

    initialize project

--- middleware/versionMiddleware.js
+++ middleware/versionMiddleware.js
@@ -1,7 +1,7 @@
-versionMiddleware = function(version) {
-  return function(req, res, next) {
+versionMiddleware = function (version) {
+  return function (req, res, next) {
     let requestVersion = parseInt(req.params.version.substring(1)); // removes the "v" and turns into a number
-    if (typeof requestVersion !== 'number') {
+    if (typeof requestVersion !== "number") {
       return next(new Error("Invalid API version requested."));
     } else if (requestVersion >= version) {
       return next();
